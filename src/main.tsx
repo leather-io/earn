ce7e953b5fd6d2e23d72fbe6f0aa3fbf250209a4
kyranjamie <kyran.burraston@gmail.com>
2024-02-14 12:27:37 +0100

    chore: trigger vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app';
 
 // Ensures `BigInt`s don't throw errors when using `JSON.stringify`, as they are not supported by
-// the `stringify` function.
+// the `stringify` function
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
