eb5d0d4054096a547710873d380519edce8e6fff
Hank Stoever <hstove@gmail.com>
2024-04-22 13:19:34 -0700

    feat: add `disabled` flag to pools and hide if disabled

--- src/pages/stacking/start-pooled-stacking/types-preset-pools.ts
+++ src/pages/stacking/start-pooled-stacking/types-preset-pools.ts
@@ -15,11 +15,13 @@ export const enum PoxContractName {
   Pox3,
   WrapperOneCycle,
   WrapperFastPool,
+  Pox4,
 }
 
 export const NetworkInstanceToPoxContractMap = {
   [NetworkInstance.devnet]: {
     [PoxContractName.Pox3]: 'ST000000000000000000002AMW42H.pox-3',
+    [PoxContractName.Pox4]: 'ST000000000000000000002AMW42H.pox-4',
     [PoxContractName.WrapperOneCycle]:
       'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.pox-pools-1-cycle',
     [PoxContractName.WrapperFastPool]:
@@ -27,6 +29,7 @@ export const NetworkInstanceToPoxContractMap = {
   },
   [NetworkInstance.testnet]: {
     [PoxContractName.Pox3]: 'ST000000000000000000002AMW42H.pox-3',
+    [PoxContractName.Pox4]: 'ST000000000000000000002AMW42H.pox-4',
     [PoxContractName.WrapperOneCycle]:
       'ST2PABAF9FTAJYNFZH93XENAJ8FVY99RRM4DF2YCW.pox-pools-1-cycle-v2',
     [PoxContractName.WrapperFastPool]:
@@ -34,6 +37,7 @@ export const NetworkInstanceToPoxContractMap = {
   },
   [NetworkInstance.mainnet]: {
     [PoxContractName.Pox3]: 'SP000000000000000000002Q6VF78.pox-3',
+    [PoxContractName.Pox4]: 'SP000000000000000000002Q6VF78.pox-4',
     [PoxContractName.WrapperOneCycle]:
       'SP001SFSMC2ZY76PD4M68P3WGX154XCH7NE3TYMX.pox-pools-1-cycle-v2',
     [PoxContractName.WrapperFastPool]: 'SP21YTSM60CAY6D011EZVEVNKXVW8FVZE198XEFFP.pox-fast-pool-v2',
