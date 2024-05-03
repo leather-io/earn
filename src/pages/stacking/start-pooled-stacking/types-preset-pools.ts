2b0161cf7b6015ebc91f4949a8c6d45befd1de97
friedger <mail@friedger.de>
2024-05-03 10:56:51 +0200

    feat: add restake pool, replace fast pool logo

--- src/pages/stacking/start-pooled-stacking/types-preset-pools.ts
+++ src/pages/stacking/start-pooled-stacking/types-preset-pools.ts
@@ -8,6 +8,7 @@ export const enum PoolName {
   FastPool = 'FAST Pool',
   Xverse = 'Xverse',
   PlanBetter = 'PlanBetter',
+  Restake = 'Restake',
   CustomPool = 'Custom Pool',
 }
 
@@ -15,6 +16,7 @@ export const enum PoxContractName {
   Pox3,
   WrapperOneCycle,
   WrapperFastPool,
+  WrapperRestake,
   Pox4,
 }
 
@@ -25,6 +27,7 @@ export const NetworkInstanceToPoxContractMap = {
     [PoxContractName.WrapperOneCycle]: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.pox4-pools',
     [PoxContractName.WrapperFastPool]:
       'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.pox4-self-service',
+    [PoxContractName.WrapperRestake]: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.pox4-self-service',
   },
   [NetworkInstance.testnet]: {
     [PoxContractName.Pox3]: 'ST000000000000000000002AMW42H.pox-3',
@@ -32,6 +35,7 @@ export const NetworkInstanceToPoxContractMap = {
     [PoxContractName.WrapperOneCycle]: 'ST2PABAF9FTAJYNFZH93XENAJ8FVY99RRM4DF2YCW.pox4-pools',
     [PoxContractName.WrapperFastPool]:
       'ST2PABAF9FTAJYNFZH93XENAJ8FVY99RRM4DF2YCW.pox4-self-service',
+    [PoxContractName.WrapperRestake]: 'ST2PABAF9FTAJYNFZH93XENAJ8FVY99RRM4DF2YCW.pox4-self-service',
   },
   [NetworkInstance.mainnet]: {
     [PoxContractName.Pox3]: 'SP000000000000000000002Q6VF78.pox-3',
@@ -39,6 +43,8 @@ export const NetworkInstanceToPoxContractMap = {
     [PoxContractName.WrapperOneCycle]: 'SP001SFSMC2ZY76PD4M68P3WGX154XCH7NE3TYMX.pox4-pools',
     [PoxContractName.WrapperFastPool]:
       'SP21YTSM60CAY6D011EZVEVNKXVW8FVZE198XEFFP.pox4-fast-pool-v3',
+    [PoxContractName.WrapperRestake]:
+      'SPZV5RJN5XTJHA76E0VHEFB0WPEH7E11NZZ4CGBK.restake-self-service-pool-v1',
   },
 } as const;
 
