310e4e928f43d67428923c96623e4237abfd3d9a
Hank Stoever <hstove@gmail.com>
2024-05-14 10:54:32 -0700

    fix: use "pox-4" in paragraph

--- src/pages/stacking/pool-admin/pool-info/pool-info.tsx
+++ src/pages/stacking/pool-admin/pool-info/pool-info.tsx
@@ -49,7 +49,7 @@ export function PoolInfo() {
           estimatedStackingMinimum={BigInt(getPoxInfoQuery.data.min_amount_ustx)}
           timeUntilNextCycle={getSecondsUntilNextCycleQuery.data}
         >
-          Managing a stacking pool usually happens directly through the pox-3 contract. For each
+          Managing a stacking pool usually happens directly through the pox-4 contract. For each
           pool member, who delegated to your pool address, you have to stack their stack and later
           extend and/or increase their locked tokens.
           <br />
