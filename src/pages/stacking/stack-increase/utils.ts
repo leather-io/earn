6fa9bac1779e96e1e374237c6b3da7e593aafdf0
Hank Stoever <hstove@gmail.com>
2024-09-26 12:31:16 -0700

    fix: add signature validation to stack-increase

--- src/pages/stacking/stack-increase/utils.ts
+++ src/pages/stacking/stack-increase/utils.ts
@@ -1,18 +1,28 @@
 import { NavigateFunction } from 'react-router-dom';
 
 import { ContractCallRegularOptions, showContractCall } from '@stacks/connect';
-import { AccountExtendedBalances, StackingClient } from '@stacks/stacking';
+import {
+  AccountExtendedBalances,
+  Pox4SignatureTopic,
+  StackerInfo,
+  StackingClient,
+  verifyPox4SignatureHash,
+} from '@stacks/stacking';
 import BigNumber from 'bignumber.js';
 import * as yup from 'yup';
 
 import routes from '@constants/routes';
+import { StacksNetworkContext } from '@hooks/use-stacks-network';
 import { validateDecimalPrecision } from '@utils/form/validate-decimals';
+import { formatPoxAddressToNetwork } from '@utils/stacking';
 import { stxToMicroStx, stxToMicroStxBigint } from '@utils/unit-convert';
+import { hexStringSchema } from '@utils/validators/hex-string-validator';
 import { stxAmountSchema } from '@utils/validators/stx-amount-validator';
 import { stxBalanceValidator } from '@utils/validators/stx-balance-validator';
 
 import { StackIncreaseInfo } from '../direct-stacking-info/get-has-pending-stack-increase';
 import { SignerDetailsFormValues } from '../pool-admin/stack-aggregation-commit/types';
+import { SignatureDataSchema } from '../signer/generate-signature/types';
 
 export interface EditingFormValues extends SignerDetailsFormValues {
   increaseBy: BigNumber;
@@ -28,12 +38,33 @@ interface CreateValidationSchemaArgs {
    * The stacking transaction's estimated fee. Used to ensure the account has enough STX available to both stack the desired amount and pay for the stacking transaction fee.
    */
   transactionFeeUStx: bigint;
+
+  /**
+   * Current stacker info
+   */
+  stackerInfo: StackerInfo;
+  /**
+   * The Stacks network context returned from `useStacksNetwork`.
+   */
+  network: StacksNetworkContext;
+
+  /**
+   * The current reward cycle
+   */
+  rewardCycleId: number;
 }
 
 export function createValidationSchema({
   availableBalanceUStx,
   transactionFeeUStx,
+  network,
+  rewardCycleId,
+  stackerInfo,
 }: CreateValidationSchemaArgs) {
+  if (!stackerInfo.stacked) {
+    throw new Error('User is not stacked - cannot increase');
+  }
+  const lockPeriod = stackerInfo.details.lock_period;
   return yup.object().shape({
     increaseBy: stxAmountSchema()
       .test(stxBalanceValidator(BigInt(availableBalanceUStx.toString())))
@@ -53,6 +84,63 @@ export function createValidationSchema({
           );
         },
       }),
+    signerSignature: hexStringSchema()
+      .test('matches-topic', 'Signature was not generated for stack-stx', function (_signature) {
+        const signatureJSON = this.parent.signatureJSON;
+        if (typeof signatureJSON !== 'string') return true;
+        const signatureData = SignatureDataSchema.json().cast(signatureJSON);
+        return signatureData.method === 'stack-increase';
+      })
+      .test(
+        'matches-lock-period',
+        `"Period" argument when generating signature must match current lock period (${lockPeriod})`,
+        function (_signature) {
+          const signatureJSON = this.parent.signatureJSON;
+          if (typeof signatureJSON !== 'string') return true;
+          const signatureData = SignatureDataSchema.json().cast(signatureJSON);
+          if (parseInt(signatureData.period, 10) !== stackerInfo.details.lock_period) {
+            return this.createError({
+              message: `"Period" argument when generating signature was ${signatureData.period}, but it must match current lock period (${lockPeriod})`,
+              path: 'signerSignature',
+            });
+          }
+          return true;
+        }
+      )
+      .test('valid-signature', 'Unable to validate signature', function (signerSignature, context) {
+        const signatureJSON = context.parent.signatureJSON;
+        if (typeof signatureJSON !== 'string') return true;
+        if (typeof signerSignature !== 'string') return true;
+        const signatureData = SignatureDataSchema.json().cast(signatureJSON);
+        const poxAddress = formatPoxAddressToNetwork(
+          network.network,
+          stackerInfo.details.pox_address
+        );
+        const signatureVerificationOptions = {
+          topic: 'stack-increase' as Pox4SignatureTopic,
+          rewardCycle: parseInt(signatureData.rewardCycle, 10),
+          poxAddress: poxAddress,
+          authId: context.parent.authId,
+          network: network.network,
+          publicKey: context.parent.signerKey,
+          signature: signerSignature,
+          period: stackerInfo.details.lock_period,
+          maxAmount: stxToMicroStxBigint(context.parent.maxAmount),
+        };
+        const isValid = verifyPox4SignatureHash(signatureVerificationOptions);
+        return isValid;
+      })
+      .test(
+        'matches-reward-cycle',
+        'Signature is not valid for current reward cycle',
+        function (signerSignature, context) {
+          const signatureJSON = context.parent.signatureJSON;
+          if (typeof signatureJSON !== 'string') return true;
+          if (typeof signerSignature !== 'string') return true;
+          const signatureData = SignatureDataSchema.json().cast(signatureJSON);
+          return parseInt(signatureData.rewardCycle, 10) === rewardCycleId;
+        }
+      ),
   });
 }
 
