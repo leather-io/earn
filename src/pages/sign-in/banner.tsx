f43cc3a22c2cdee16cb634fb4f359997893f7697
Will Cameron <camerow@users.noreply.github.com>
2024-12-05 11:40:36 -0800

    draft: fix banner

--- src/pages/sign-in/banner.tsx
+++ src/pages/sign-in/banner.tsx
@@ -1,23 +1,33 @@
 import { ErrorCircleIcon } from '@leather.io/ui';
-import { Flex } from 'leather-styles/jsx';
-import { token } from 'leather-styles/tokens';
-
+import { Box, Flex, styled } from 'leather-styles/jsx';
+import { css } from 'leather-styles/css';
 export function Banner() {
   return (
-    <Flex
-      color={token('colors.ink.text-primary')}
-      backgroundColor={'#D9CFC4'}
-      fontSize="13px"
-      textAlign="left"
-      alignItems="center"
-      textStyle="body.03"
-      height="80px"
-      px="24px"
-      py="8px"
+    <Box
+      className={css({
+        width: '100%',
+        backgroundColor: '#D9CFC4',
+      })}
     >
-      <ErrorCircleIcon style={{ marginRight: '8px' }} />
-      This website provides the interface to connect with the Stacking protocol or delegate to a
-      Stacking pool provider directly. We don&apos;t provide the Stacking service ourselves.
-    </Flex>
+      <Flex
+        color="ink.text-primary"
+        alignItems="center"
+        justifyContent="center"
+        className={css({ width: { base: '84%', mdDown: '90%' }, height: '80px' })}
+      >
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          maxWidth="1240px"
+          className={css({ marginLeft: { lgDown: 'space.04' } })}
+        >
+          <ErrorCircleIcon className={css({ minWidth: '14px', height: '14px' })} />
+          <styled.p textStyle="body.02" display="flex" alignItems="center">
+            This website provides the interface to connect with the Stacking protocol or delegate to
+            a Stacking pool provider directly. We don&apos;t provide the Stacking service ourselves.
+          </styled.p>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
