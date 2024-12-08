6900931a2233a563947c1c91b0a93e1483f8f46b
fabric-8 <hello@fabric8.de>
2024-12-08 21:52:34 +0100

    fix: Align banner formatting closer with design

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,31 +4,25 @@ import { Box, Flex, styled } from 'leather-styles/jsx';
 
 export function Banner() {
   return (
-    <Box
-      className={css({
-        width: '100%',
-        backgroundColor: '#D9CFC4',
-      })}
+    <Flex
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="#D9CFC4"
     >
       <Flex
         color="ink.text-primary"
-        alignItems="center"
-        justifyContent="center"
-        className={css({ width: { base: '84%', mdDown: '90%' }, height: '80px' })}
+        width="100%"
+        maxWidth="1400px"
+        paddingY="space.03"
+        className={css({ width: { smToXl: '94%' }, height: 'auto' })}
       >
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          maxWidth="1240px"
-          className={css({ marginLeft: { lgDown: 'space.04' } })}
-        >
-          <ErrorCircleIcon className={css({ minWidth: '14px', height: '14px' })} />
-          <styled.p textStyle="body.02" display="flex" alignItems="center">
-            This website provides the interface to connect with the Stacking protocol or delegate to
-            a Stacking pool provider directly. We don&apos;t provide the Stacking service ourselves.
-          </styled.p>
-        </Flex>
+        <ErrorCircleIcon className={css({ minWidth: '14px', height: '14px' })} />
+        <styled.p textStyle="label.03" display="flex" marginLeft="space.01">
+          This website provides the interface to connect with the Stacking protocol or delegate to
+          a Stacking pool provider directly. We don&apos;t provide the Stacking service ourselves.
+        </styled.p>
       </Flex>
-    </Box>
+    </Flex>
   );
 }
