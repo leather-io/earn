37dfca46b89fd34c23c3ec4d5ae6cf2e56281e8c
kyranjamie <kyran.burraston@gmail.com>
2025-05-16 12:51:21 +0200

    feat: link to new stacking app

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,6 +1,5 @@
-import { InfoCircleIcon } from '@leather.io/ui';
-import { css } from 'leather-styles/css';
-import { Box, Flex, styled } from 'leather-styles/jsx';
+import { Link } from '@leather.io/ui';
+import { Box, Flex } from 'leather-styles/jsx';
 
 export function Banner() {
   return (
@@ -12,13 +11,19 @@ export function Banner() {
         px={['space.04', 'space.04', 'space.05']}
         alignItems="center"
         justifyContent="center"
+        textStyle="body.02"
+        fontSize="13px"
       >
-        <Flex gap="space.01" alignItems="center">
-          <InfoCircleIcon className={css({ minWidth: '14px', height: '14px' })} />
-          <styled.span textStyle="body.02" fontSize="13px" fontWeight="400">
-            Lockstacks is now Leather Earn
-          </styled.span>
-        </Flex>
+        Leather Earn is outdated and only supported for a limited time. For the latest version,
+        visit{'   '}
+        <Link
+          ml="space.01"
+          fontSize={'inherit'}
+          fontWeight="400"
+          href="https://app.leather.io/stacking"
+        >
+          leather.io
+        </Link>
       </Flex>
     </Box>
   );
